c2e546467109a401587614b4cdd331599922c2fc
ohksam <SamOh37@gmail.com>
2023-05-27 19:29:36 -0400

    sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,26 @@ class ApplicationController < ActionController::API
         render json: { message: ["Hello from Rails"] }
     end
 
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def require_logged_in
+        unless current_user
+            render json: { message: 'Unauthorized' }, status: :unauthorized 
+        end
+    end
+
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout!
+        session[:session_token] = nil
+        current_user.reset_session_token!
+        @current_user = nil
+    end
+
     private
 
     def snake_case_params
