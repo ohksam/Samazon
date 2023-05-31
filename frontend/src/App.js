92728fffb34a1570e94979a6e6ac5748f3949156
ohksam <SamOh37@gmail.com>
2023-05-30 21:28:20 -0400

    Nav componenents. styling + modal next

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,24 @@ import React from "react";
 import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
+import Navigation from "./components/Navigation";
+// import Modal from './components/Modal';
 
 function App() {
   return (
-    <Switch>
-      <Route path='/login'>
-        <LoginFormPage />
-        {/* <h1>hello?</h1> */}
-      </Route>
-      <Route path='/signup'>
-        <SignupFormPage />
-      </Route>
-    </Switch>
+    <>
+      <Navigation />
+      {/* <Modal /> */}
+      <Switch>
+        <Route path='/login'>
+          <LoginFormPage />
+          {/* <h1>hello?</h1> */}
+        </Route>
+        <Route path='/signup'>
+          <SignupFormPage />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
